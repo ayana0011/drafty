b67a4af72814c648f21c211f71285c4c38839c87
m20056@g.metro-cit.ac.jp <m20056@g.metro-cit.ac.jp>
2026-03-15 00:33:43 +0900

    Initial Commit

---
+++ DraftMaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DraftMaker
+//
+//  Created by 鹿間美海 on 2026/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DraftMaker/DraftMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftMakerApp.swift
+//  DraftMaker
+//
+//  Created by 鹿間美海 on 2026/03/15.
+//
+
+import SwiftUI
+
+@main
+struct DraftMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
